6805ac3067db431da67887af9614565f23b5fe8d
yistabraq <yistabraq@gmail.com>
2024-07-05 23:20:03 +0100

    init bytebase

---
+++ bytebase/migrations/202407050000_dml_createrbac_realms.sql
@@ -0,0 +1,3 @@
+-- Insert a row into the rbac_realms table
+INSERT INTO rbac_realms (id, description, created_by, created_at, updated_by, updated_at)
+VALUES ('00000000-0000-0000-0000-000000000000', 'Default Realm', '00000000-0000-0000-0000-000000000000', CURRENT_TIMESTAMP, '00000000-0000-0000-0000-000000000000', CURRENT_TIMESTAMP);
